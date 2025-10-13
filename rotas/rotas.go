f658b47da6adea7c5970499791e4958aacf54fb8
clarinha <clarinhag070@gmail.com>
2025-10-13 20:31:40 -0300

    ajustes no arquivo rotas

--- rotas/rotas.go
+++ rotas/rotas.go
@@ -8,11 +8,8 @@ import (
 func CarregarRotas() {
 
 	http.HandleFunc("/", controle.Index)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-}
-
-func CarregarRotasMapa() {
 	http.HandleFunc("/", controle.Mapa)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 }
+
