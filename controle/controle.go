c9456d8f2090e0806da817aa87e961763de08574
welliton <168497736+wellitondev@users.noreply.github.com>
2025-10-14 21:11:00 -0300

    alteração

--- controle/controle.go
+++ controle/controle.go
@@ -1,28 +1,30 @@
 package controle
 
 import (
+	"html/template"
 	"net/http"
-	"text/template"
 )
 
-var temp = template.Must(template.ParseGlob("templates/*.html"))
+// Carrega os templates corretamente
+var temp = template.Must(template.ParseFiles(
+	"templates/home.html",
+	"templates/mapa.html", 
+	"templates/cadastro.html",
+	"templates/relatorios.html",
+))
 
-// Home - Página inicial
 func Home(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "home.html", nil)
 }
 
-// Mapa - Página do mapa
 func Mapa(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "mapa.html", nil)
 }
 
-// Cadastro - Página de cadastro
 func Cadastro(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "cadastro.html", nil)
 }
 
-// Relatorios - Página de relatórios
 func Relatorios(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "relatorios.html", nil)
 }
